385ba54eaa76005cf00e7bd2b0370b0177f302b8
MuhammadAllouzi <mohammadal9624@gmail.com>
2025-10-19 23:02:22 +0300

    Add files via upload

---
+++ #topic 1.py
@@ -0,0 +1,50 @@
+#topic 1
+print("hello word")
+print("python","is","fun!")
+#topic2
+name="mohammad"
+age=20
+country="jordan"
+print("name:",name)
+print("age:",age)
+print("country:",country)
+#topic3
+x = 10
+y =5
+result = x + y
+print("the sum of x and y is:", result)
+x=x*2
+print("updated value of x:",x)
+print("my name",name,"and i am from",country)
+#02 topic4
+name="mohammad"
+age=20
+country="jordane"
+print(f"My anme is {name},i am {age} years old , and i live in {country}.")
+x=10
+y=5
+print(f"the sum of {x} and {y} is {x+y}.")
+pi = 3.14159265
+print(f"pi rounded to 2 decimal places :{pi:2f}.")
+print (f"|{'left':<10} | {'center':^10} | {'right':>10} |")
+#topic 5
+a=8
+b=3
+#+
+print(f"addition:{a}+{b}={a+b}")
+#*
+print(f"multiplication:{a}*{b}={a*b}")
+#-
+print(f"subtraction:{a}-{b}={a-b}")
+#/
+print(f"Division: {a}/{b} = {a/b}")
+#//
+print(f"floor division:{a}//{b}={a//b}")
+#%
+print(f"moduius(remainder):{a}%{b}={a%b}")
+#**
+print(f"exponentiation:{a}**{b}={a**b}")
+#topic6
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f"The sum is: {num1 + num2}")
